e1c7a883741f6e0dc96fc75fc1446041a05d1eb4
mesmoudi13 <m.mesmoudi@esi-sba.dz>
2020-12-16 16:00:33 +0100

    complete responsive navbar

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//change the toggle icon when clicking
+
+let burger = document.querySelector('i.fa-bars');
+let close = document.querySelector('i.fa-times');
+
+changeIcon = () =>{
+    burger.classList.toggle('d-none')
+    close.classList.toggle('d-none')
+
+}
+
