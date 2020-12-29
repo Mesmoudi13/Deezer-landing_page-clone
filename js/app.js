632643e9b71c75bfdd5d1c930efb769959eefbc3
mesmoudi13 <m.mesmoudi@esi-sba.dz>
2020-12-29 16:35:08 +0100

    finishing the footer + some edit

--- js/app.js
+++ js/app.js
@@ -9,17 +9,47 @@ changeIcon = () =>{
 
 }
 
-//Show more function in the Footer
+//Show more in the Footer
+
 let links = document.getElementsByClassName('footer__links')
-let plus = document.getElementsByClassName('fa-plus')
-let minus = document.getElementsByClassName('fa-minus')
-plus.addEventListener("click", showMore())
+let plus = document.getElementsByClassName("fa-plus")
+let minus = document.getElementsByClassName("fa-minus")
 
-showMore = () =>{
-    links.style.display = block;
-    plus.classList.remove('d-block');
-    plus.classList.add('d-none')
-    minus.classList.add('d-block')
-    minus.classList.remove('d-none')
 
+showMore = (e,n,i) => {
+    do {
+        links[i].classList.remove("d-none");
+        links[i].classList.add("d-block");
+        i++;
+    }
+    while (links[i].classList.contains("not"));
+    links[i].classList.remove("d-none");
+    links[i].classList.add("d-block");
+    e.classList.add("d-none");
+    e.classList.remove("d-block");
+    n.classList.remove("d-none");
+    n.classList.add("d-block");
 }
+
+//Show less in the footer
+
+showLess = (e,n,i) =>{
+    do {
+        links[i].classList.remove("d-block");
+        links[i].classList.add("d-none");
+        i++;
+    }
+    while (links[i].classList.contains("not"));
+    links[i].classList.remove("d-block");
+    links[i].classList.add("d-none");
+    e.classList.add("d-block");
+    e.classList.remove("d-none");
+    n.classList.remove("d-block");
+    n.classList.add("d-none");
+}
+
+
+
+
+
+
